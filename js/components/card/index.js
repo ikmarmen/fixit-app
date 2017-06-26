2ee4f9014c4ec6d4f54dca0b1a6276ebe2fa65b8
armen.sargsyan <armen.sargsyan@margasoft.com>
2017-06-26 16:32:43 +0400

    test

--- js/components/card/index.js
+++ js/components/card/index.js
@@ -1,38 +1,65 @@
 import React, { Component } from 'react';
 import { Image } from 'react-native';
-import { Card, CardItem, Thumbnail, Body, Text, Left, Icon, Button, DeckSwiper, View } from 'native-base';
+import { Card, CardItem, Thumbnail, Body, Text, Left, Right, Icon, Item, Button, DeckSwiper, View } from 'native-base';
 import { Actions, ActionConst } from 'react-native-router-flux';
-import {timeSince} from '../../utils/dateHelper';
+import { timeSince } from '../../utils/dateHelper';
 
 export default class FixItCard extends Component {
   constructor(props) {
     super(props);
     this.store = this.props.advert;
   }
+
+  _photo = (photos) => {
+    try {
+      /*if (photos.length > 0) {
+        let chars = [];
+        for (let i = 0; i < photos[0].data.data.length; i++) {
+          chars.push(String.fromCharCode(photos[0].data.data[i]))
+        }
+        let base64str = btoa(chars.join(''));
+
+        return 'data:image/jpeg;base64,' + base64str;
+      }*/
+    }
+    catch (error)
+    {
+      let x = 54;
+    }
+    
+    return null;
+
+  }
+
   render() {
     return <Card>
       <CardItem>
         <Left>
-          <Thumbnail source={{ uri: this.store.advert.photos[0] }} />
+          <Thumbnail source={{ uri: this._photo(this.store.advert.photos) }}/>
           <Body>
             <Text>{this.store.advert.title}</Text>
-            <Text note>{this.store.advert.userName}</Text>
+            <Text note>{this.store.advert.userId}</Text>
           </Body>
         </Left>
       </CardItem>
       <CardItem cardBody>
-        <Image source={{ uri: this.store.advert.photos[0] }} style={{ height: 200, width: null, flex: 1 }} />
+        <Image source={{ uri: this._photo(this.store.advert.photos) }} style={{ height: 200, width: null, flex: 1 }} />
       </CardItem>
       <CardItem>
-        <Button transparent>
-          <Icon active name="cash" />
-          <Text>{this.store.advert.bids.length + ' bids'}</Text>
-        </Button>
-        <Button transparent>
-          <Icon active name="chatbubbles" />
-          <Text>4 Comments</Text>
-        </Button>
-        <Text>{timeSince(this.store.advert.createdAt)}</Text>
+        <Left>
+          <Button transparent>
+            <Icon active name="cash" />
+            <Text>{this.store.advert.bids.length.toString() + ' bids'}</Text>
+          </Button>
+          <Button transparent>
+            <Icon active name="chatbubbles" />
+            <Text>4 comments</Text>
+          </Button>
+        </Left>
+        <Right>
+          <Text>{this.store.advert.distance + ' miles'}</Text>
+          <Text>{timeSince(this.store.advert.createdAt) + ' ago'}</Text>
+        </Right>
       </CardItem>
     </Card>;
   }
