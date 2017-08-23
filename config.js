1bd1f82a0ff0db7147830efc104201d08eeb37bf
armen.sargsyan <sargsarmen@gmail.com>
2017-08-23 18:04:06 +0400

    Added Linux server ip

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var _config = {
   production: { BASE_URL: '', APP_VERSION: '0.1.0' },
-  staging: { BASE_URL: '', APP_VERSION: '' },
-  dev: { BASE_URL: 'http://192.168.10.116:4000/', APP_VERSION: '0.1.0' },
+  staging: { BASE_URL: 'http://192.168.10.116:4000/', APP_VERSION: '' },//Armen S. IP
+  dev: { BASE_URL: 'http://192.168.139.146:80/', APP_VERSION: '0.1.0' },
 }
 
 function getConfig() {
