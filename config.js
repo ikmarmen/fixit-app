800c251678a68bd15b6c9e22eef8b4199ad20c65
armen.sargsyan <sargsarmen@gmail.com>
2017-11-17 12:41:10 +0400

    change ip in config

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 var _config = {
   production: { BASE_URL: '', APP_VERSION: '0.1.0' },
-  staging: { BASE_URL: 'http://192.168.10.116:4000/', APP_VERSION: '0.1.0' },//Armen S. IP
-  dev: { BASE_URL: 'http://192.168.10.30:4000/', APP_VERSION: '0.1.0' },
+  staging: { BASE_URL: 'http://138.201.239.237:4000/', APP_VERSION: '0.1.0' },//Armen S. IP
+  dev: { BASE_URL: 'http://192.168.10.116:4000/', APP_VERSION: '0.1.0' },
 }
 
 function getConfig() {
   //Todo: Make mechanism to recognize envirment
-  return _config['staging']
+  return _config['dev']
 }
 var Config = getConfig();
 export default Config
