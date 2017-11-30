2692e8085f4783be928716eb94b341c1f86e45be
armen.sargsyan <sargsarmen@gmail.com>
2017-11-30 15:53:27 +0400

    Updated project dependencies

--- android/app/src/main/java/com/fixit/MainApplication.java
+++ android/app/src/main/java/com/fixit/MainApplication.java
@@ -3,6 +3,7 @@ package com.fixit;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.marianhello.react.BackgroundGeolocationPackage;
@@ -32,6 +33,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNGoogleSigninPackage(),
             new PickerPackage(),
             new RCTCameraPackage(),
             new RNFetchBlobPackage(),
--- js/components/auth/login.js
+++ js/components/auth/login.js
@@ -1,9 +1,18 @@
-import React, { Component } from 'react';
-import { observer } from 'mobx-react';
-import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, KeyboardAvoidingView } from 'react-native'
-import { LoginStore, AuthStore } from './store';
-import { Actions, ActionConst } from 'react-native-router-flux';
-import FloatingLabelInput from '../../controls/floatingLabelInput';
+import React, { Component } from "react";
+import { observer } from "mobx-react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView
+} from "react-native";
+import { LoginStore, AuthStore } from "./store";
+import { Actions, ActionConst } from "react-native-router-flux";
+import FloatingLabelInput from "../../controls/floatingLabelInput";
+//import { GoogleSignin } from "react-native-google-signin";
 
 @observer
 export default class Login extends Component {
@@ -13,85 +22,108 @@ export default class Login extends Component {
   }
 
   onLogin = () => {
-    let request = { email: this.store.email, password: this.store.password }
-    AuthStore.login(request)
-  }
+    let request = { email: this.store.email, password: this.store.password };
+    AuthStore.login(request);
+  };
 
   onSignUp = () => {
     Actions.signup({ type: ActionConst.PUSH });
-  }
+  };
 
   onForgotPassword = () => {
     Actions.forgotPassword({ type: ActionConst.PUSH });
-  }
+  };
 
   onGoogleLogin = () => {
-  }
+   /*  GoogleSignin.hasPlayServices({ autoResolve: true })
+      .then(() => {
+        // play services are available. can now configure library
+      })
+      .catch(err => {
+        console.log("Play services error", err.code, err.message);
+      }); */
+  };
 
-  onFacebookLogin = () => {
-  }
+  onFacebookLogin = () => {};
 
   render() {
     return (
       <View style={styles.container}>
         <Image
           style={styles.logoImage}
-          source={require('../../../img/logo.png')}
+          source={require("../../../img/logo.png")}
         />
-        <KeyboardAvoidingView style={styles.inputContainer} behavior={'position'} keyboardVerticalOffset={5}>
+        <KeyboardAvoidingView
+          style={styles.inputContainer}
+          behavior={"position"}
+          keyboardVerticalOffset={5}
+        >
           <FloatingLabelInput
             inputStyle={StyleSheet.flatten(styles.input)}
             label={"Email"}
             value={this.store.email}
-            onChangeText={(text) => this.store.setProp(text, 'email')} />
+            onChangeText={text => this.store.setProp(text, "email")}
+          />
           <FloatingLabelInput
             inputStyle={StyleSheet.flatten(styles.input)}
             label={"Password"}
             password
             value={this.store.password}
-            onChangeText={(text) => this.store.setProp(text, 'password')} />
+            onChangeText={text => this.store.setProp(text, "password")}
+          />
         </KeyboardAvoidingView>
 
-
-        <TouchableOpacity style={styles.LoginButtoncContainer} activeOpacity={0.5} onPress={this.onLogin} >
+        <TouchableOpacity
+          style={styles.LoginButtoncContainer}
+          activeOpacity={0.5}
+          onPress={this.onLogin}
+        >
           <View style={styles.btnContainer}>
             <Text style={styles.btnText}>LOG IN</Text>
           </View>
-        </TouchableOpacity >
-        <TouchableOpacity activeOpacity={0.5} onPress={this.onForgotPassword} >
+        </TouchableOpacity>
+        <TouchableOpacity activeOpacity={0.5} onPress={this.onForgotPassword}>
           <View>
             <Text style={styles.linkText}>Forgot password?</Text>
           </View>
-        </TouchableOpacity >
+        </TouchableOpacity>
 
         <View style={styles.socialLoginsContainer}>
-          <TouchableOpacity style={styles.btnSocContainer} activeOpacity={0.5} onPress={this.onFacebookLogin}>
+          <TouchableOpacity
+            style={styles.btnSocContainer}
+            activeOpacity={0.5}
+            onPress={this.onFacebookLogin}
+          >
             <View style={styles.btnContainerTransparent}>
               <Image
                 style={styles.btnIconFb}
-                source={require('../../../img/fb.png')}
+                source={require("../../../img/fb.png")}
               />
               <Text style={styles.socBtnText}>Log in with Facebook</Text>
             </View>
-          </TouchableOpacity >
-          <TouchableOpacity style={styles.btnSocContainer} activeOpacity={0.5} onPress={this.onGoogleLogin}>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.btnSocContainer}
+            activeOpacity={0.5}
+            onPress={this.onGoogleLogin}
+          >
             <View style={styles.btnContainerTransparent}>
               <Image
                 style={styles.btnIconGoogle}
-                source={require('../../../img/google.png')}
+                source={require("../../../img/google.png")}
               />
               <Text style={styles.socBtnText}>Log in with Google</Text>
             </View>
           </TouchableOpacity>
         </View>
 
-        <View style={styles.bottomText} >
+        <View style={styles.bottomText}>
           <Text style={styles.linkText}>New to FIX IT?</Text>
-          <TouchableOpacity activeOpacity={0.5} onPress={this.onSignUp} >
+          <TouchableOpacity activeOpacity={0.5} onPress={this.onSignUp}>
             <View>
               <Text style={styles.linkTextRight}>SIGN UP</Text>
             </View>
-          </TouchableOpacity >
+          </TouchableOpacity>
         </View>
       </View>
     );
@@ -101,97 +133,97 @@ export default class Login extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    backgroundColor: '#264559',
+    justifyContent: "flex-start",
+    alignItems: "center",
+    backgroundColor: "#264559"
   },
   input: {
-    color: 'white'
+    color: "white"
   },
   inputLabel: {
-    color: 'white',
-    fontSize: 12,
+    color: "white",
+    fontSize: 12
   },
   inputText: {
-    color: 'white',
-    fontSize: 18,
+    color: "white",
+    fontSize: 18
   },
   logoImage: {
     width: 75,
     height: 75,
-    marginTop: '20%',
+    marginTop: "20%"
   },
   linkText: {
-    color: '#ffffff',
-    opacity: .5,
+    color: "#ffffff",
+    opacity: 0.5,
     marginTop: 10,
-    marginBottom: 50,
+    marginBottom: 50
   },
   inputContainer: {
-    width: '75%',
+    width: "75%",
     height: 140,
-    marginTop: '14%',
+    marginTop: "14%"
   },
   LoginButtoncContainer: {
-    width: '75%',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: "75%",
+    alignItems: "center",
+    justifyContent: "center"
   },
   btnContainer: {
-    width: '100%',
+    width: "100%",
     height: 50,
     borderRadius: 25,
-    alignItems: 'center',
-    backgroundColor: '#e5e642',
-    justifyContent: 'center',
+    alignItems: "center",
+    backgroundColor: "#e5e642",
+    justifyContent: "center"
   },
   btnIconFb: {
     height: 12,
     width: 8,
-    marginRight: 2,
+    marginRight: 2
   },
   btnIconGoogle: {
     height: 12,
     width: 11,
-    marginRight: 3,
+    marginRight: 3
   },
   btnText: {
     fontSize: 18,
-    color: 'white',
-    fontWeight: '800',
-    backgroundColor: 'transparent',
+    color: "white",
+    fontWeight: "800",
+    backgroundColor: "transparent"
   },
   socialLoginsContainer: {
-    width: '75%',
-    flexDirection: 'row',
-    marginTop: '3%',
-    justifyContent: 'space-between',
+    width: "75%",
+    flexDirection: "row",
+    marginTop: "3%",
+    justifyContent: "space-between"
   },
   btnSocContainer: {
-    width: '50%',
+    width: "50%"
   },
   btnContainerTransparent: {
-    width: '95%',
+    width: "95%",
     height: 40,
     borderRadius: 25,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'rgba(255, 255, 255, 0.3)',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "rgba(255, 255, 255, 0.3)"
   },
   socBtnText: {
     fontSize: 12,
-    color: 'white',
+    color: "white"
   },
   bottomText: {
-    marginTop: '2%',
-    flexDirection: 'row',
+    marginTop: "2%",
+    flexDirection: "row"
   },
   linkTextRight: {
-    color: '#ffffff',
+    color: "#ffffff",
     marginLeft: 10,
     marginBottom: 50,
     marginRight: 10,
-    marginTop: 10,
-  },
+    marginTop: 10
+  }
 });
